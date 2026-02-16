862a2fd71e15868ffe183706cbcb27d7d4e1099b
hasibul islam <hasibulislamha01@gmail.com>
2026-02-16 13:48:11 +0600

    product cards added

--- index.js
+++ index.js
@@ -51,6 +51,7 @@ function displayCats(cats) {
             "rounded-full",
             "font-semibold"
         )
+        catBadge.onclick(()=>filterProducts(cat))
         catContainer.append(catBadge)
     });
 }
@@ -65,22 +66,45 @@ function displayProducts(products) {
         const prodCard = document.createElement("div")
         prodCard.innerHTML = `
         <div class="card bg-base-100 w-80 shadow-sm">
-  <figure>
-    <img
-      src=${product?.image}
-      alt=${product?.title}
-      class="px-4 pt-6 w-60 h-60 object-contain" 
-      />
-  </figure>
-  <div class="card-body">
-    <h2 class="card-title">${prodName}</h2>
-    <p>${description}</p>
-    <div class="card-actions flex items-center justify-evenly">
-      <button class="btn btn-primary">Details</button>
-      <button class="btn btn-primary">ATC</button>
-    </div>
-  </div>
-</div>
+            <figure>
+                <img
+                src=${product?.image}
+                alt=${product?.title}
+                class="px-4 pt-6 w-60 h-60 object-contain" 
+                />
+            </figure>
+            <div class="card-body">
+                <div class="flex items-center justify-between">
+                    <div class="badge badge-soft badge-info">${product?.category}</div>
+                    <div>
+                        <i class="fa-solid fa-star" style="color: rgba(230, 178, 26, 1.00);"></i>
+                        ${product?.rating?.rate} (${product?.rating?.count})
+                    </div>
+                </div>
+                <h2 class="card-title">${prodName}</h2>
+                <p>${description}</p>
+                <div class="card-actions mt-3 flex items-center justify-evenly *:h-8 *:w-32">
+                    
+                    
+                    <!-- Open the modal using ID.showModal() method -->
+                        <button class="btn btn-outline text-gray-600 hover:text-black hover:scale-95 active:scale-90 transition-all duration-200" onclick="my_modal_2.showModal()">
+                            <i class="fa-regular fa-eye";"></i>
+                            Details
+                        </button>
+                        <dialog id="my_modal_2" class="modal">
+                        <div class="modal-box">
+                            <h3 class="text-lg font-bold">Hello!</h3>
+                            <p class="py-4">Press ESC key or click outside to close</p>
+                        </div>
+                        <form method="dialog" class="modal-backdrop">
+                            <button>close</button>
+                        </form>
+                        </dialog>
+
+                    <button class="btn bg-[rgb(var(--primary))] text-white hover:scale-95 active:scale-90 transition-all duration-200">ATC</button>
+                </div>
+            </div>
+        </div>  
         `
         prodContainer?.append(prodCard)
     })

904179b6ec37d8cb84b619d256067337025511c7
hasibul islam <hasibulislamha01@gmail.com>
2026-02-17 15:25:25 +0600

    fixed modal

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ async function loadTopRated() {
         const firstTopRated = sortedTopRated?.slice(0, 3)
         displayTopRated(firstTopRated)
     } catch (error) {
-       console.log("Failed to load top Products", error)
+        console.log("Failed to load top Products", error)
     }
 }
 
@@ -139,7 +139,43 @@ function displayCats(cats) {
 const displayModal = (product) => {
     console.log("found product with id ", product)
     const detailsBox = document.getElementById("detailsContainer")
+    detailsBox.innerHTML = `
+    <div class="space-y-3">
+        <div
+                    class="px-5 flex flex-col md:flex-row items-center justify-evenly gap-4">
+                    <div class="flex flex-col justify-between gap-5">
+                        <img class="flex-1 object-cover max-w-40 rounded-md"
+                            src=${product?.image} />
+                        <button class="btn btn-xs sm:btn-sm btn-primary">Add to cart</button>
+                    </div>
 
+                    <div class="h-full flex flex-col justify-between gap-3">
+
+                        <div class="badge badge-soft badge-info">${product?.category}</div>
+                        <div class="space-y-2">
+                            <h1 class="text-lg font-bold">${product?.title}</h1>
+
+                            <div class="flex items-center gap-3">
+                                <div class="rating rating-xs">
+                                    <div class="mask mask-star bg-orange-500" name="rating-2" aria-label="1 star"></div>
+                                    <div class="mask mask-star bg-orange-500" name="rating-2" aria-label="2 star"></div>
+                                    <div class="mask mask-star bg-orange-500" name="rating-2" aria-label="3 star"
+                                        aria-current="true"></div>
+                                    <div class="mask mask-star bg-orange-500" name="rating-2" aria-label="4 star"></div>
+                                    <div class="mask mask-star bg-orange-500" name="rating-5" aria-label="5 star"></div>
+                                </div>
+                                <p>${product?.rating?.count}</p>
+                            </div>
+                            <h3 class="text-5xl font-bold">${product?.price}</h3>
+                        </div>
+
+
+                        <!-- <h3>Rating: 4.7</h3> -->
+                    </div>
+                </div>
+                <p>${product?.description}</p>
+    </div>
+    `
     document.getElementById("my_modal_5").showModal()
 }
 

0a9b0601677e6c1bc951d7b8987ce09fb9e5486a
hasibul islam <hasibulislamha01@gmail.com>
2026-02-17 10:58:23 +0600

    top rated section added

--- index.js
+++ index.js
@@ -17,6 +17,21 @@ async function loadCategories() {
     }
 }
 
+async function loadTopRated() {
+    const url = "https://fakestoreapi.com/products"
+    try {
+        const response = await fetch(url)
+        const data = await response.json()
+
+        const topRatedProducts = data?.filter(product => product?.rating?.rate > 4.5)
+        const sortedTopRated = topRatedProducts?.sort((a, b) => b?.rating?.rate - a?.rating?.rate)
+        const firstTopRated = sortedTopRated?.slice(0, 3)
+        displayTopRated(firstTopRated)
+    } catch (error) {
+       console.log("Failed to load top Products", error)
+    }
+}
+
 async function loadProducts(category = "all") {
     let url = "https://fakestoreapi.com/products"
     if (category !== "all") { url = `https://fakestoreapi.com/products/category/${category}` }
@@ -25,6 +40,8 @@ async function loadProducts(category = "all") {
         const data = await response.json()
         console.log("products fetched succesfully")
         displayProducts(data)
+        loadTopRated()
+
     } catch (error) {
         console.log("Failed to load Products", error)
     }
@@ -43,8 +60,10 @@ async function loadSpecificProduct(id) {
     }
 }
 
+// Documents selection using js
 const catContainer = document.getElementById("catContainer")
 const prodContainer = document.getElementById("prodContainer")
+const topRatedContainer = document.getElementById("topRatedContainer")
 
 
 /**
@@ -52,6 +71,51 @@ const prodContainer = document.getElementById("prodContainer")
  * ---------DATA >>>>   DISPLAYER   <<< FUNCTIONS ------------
  * -----------------------------------------------------------
  */
+function displayTopRated(topRatedProducts) {
+    topRatedContainer.innerHTML = ""
+    console.log(topRatedProducts, topRatedContainer)
+    topRatedProducts?.forEach(product => {
+        const prodName = product?.title?.length > 25 ? `${product?.title?.slice(0, 25)} . . .` : product?.title
+        const description = product?.description?.length > 100 ? `${product?.description?.slice(0, 100)} ....` : product?.description
+        const topRatedCard = document?.createElement("div")
+        topRatedCard.innerHTML = `
+    <div class="card bg-base-100 w-80 shadow-sm">
+            <figure>
+                <img
+                src=${product?.image}
+                alt=${product?.title}
+                class="px-4 pt-6 w-60 h-60 object-contain" 
+                />
+            </figure>
+            <div class="card-body">
+                <div class="flex items-center justify-between">
+                    <div class="badge badge-soft badge-info">${product?.category}</div>
+                    <div>
+                        <i class="fa-solid fa-star" style="color: rgba(230, 178, 26, 1.00);"></i>
+                        ${product?.rating?.rate} (${product?.rating?.count})
+                    </div>
+                </div>
+                <h2 class="card-title">${prodName}</h2>
+                <p>${description}</p>
+                <div class="card-actions mt-3 flex items-center justify-evenly *:h-8 *:w-32">
+                    
+                    <button
+        class="btn btn-outline text-gray-600 hover:text-black hover:scale-95 active:scale-90 transition-all duration-200"
+        onclick="loadSpecificProduct(${product?.id})">
+        <i class="fa-regular fa-eye"></i>
+        Details
+    </button>                    
+
+                    <button class="btn bg-[rgb(var(--primary))] text-white hover:scale-95 active:scale-90 transition-all duration-200" onclick="()=>addToCart(${product})">ATC</button>
+                </div>
+            </div>
+        </div>
+    `
+        topRatedContainer?.append(topRatedCard)
+    })
+
+
+}
 
 function displayCats(cats) {
     catContainer.innerHTML = ""
@@ -75,7 +139,7 @@ function displayCats(cats) {
 const displayModal = (product) => {
     console.log("found product with id ", product)
     const detailsBox = document.getElementById("detailsContainer")
-    
+
     document.getElementById("my_modal_5").showModal()
 }
 
